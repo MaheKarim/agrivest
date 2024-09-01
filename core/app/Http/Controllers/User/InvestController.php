206fe6898af6ecc9989f1ed33b554643a58f0d65
Mahe Karim <mahekarim@gmail.com>
2024-09-01 22:02:50 +0600

    :WIP: InvestController Ongoing :rocket:

--- core/app/Http/Controllers/User/InvestController.php
+++ core/app/Http/Controllers/User/InvestController.php
@@ -19,13 +19,16 @@ class InvestController extends Controller
         ]);
 
        $project = Project::find($request->project_id);
+
         if ($request->quantity > $project->available_share) {
             $notify[] = ['error', 'Not enough shares available.'];
             return back()->withNotify($notify);
         }
+
         $unitPrice = $project->share_amount;
         $totalPrice = $unitPrice * $request->quantity;
         $totalEarning = ($request->quantity * ($project->share_amount + $project->roi_amount));
+        $totalShare = $project->share_count;
 
         $invest = new Invest();
         $invest->user_id = auth()->id();
@@ -37,6 +40,7 @@ class InvestController extends Controller
         $invest->roi_amount = $project->roi_amount;
         $invest->payment_type = $request->payment_type;
         $invest->total_earning = $totalEarning;
+        $invest->total_share = $totalShare;
         $invest->save();
 
         return redirect()->route('user.home')->with('success', 'Investment has been placed successfully');
